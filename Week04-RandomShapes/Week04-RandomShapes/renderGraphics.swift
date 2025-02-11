9060c27f335bc9ef61fbca7d8efcd6c15b51a3a1
John Henry Thompson <jht9629@nyu.edu>
2025-02-11 09:03:05 -0500

    Update renderGraphics.swift

--- Week04-RandomShapes/Week04-RandomShapes/renderGraphics.swift
+++ Week04-RandomShapes/Week04-RandomShapes/renderGraphics.swift
@@ -10,28 +10,28 @@ import UIKit
 // from https://github.com/molab-itp/01-UIRender-playground
 
 func renderGraphics(_ width:Int, _ height:Int) -> UIImage {
-    print("renderGraphics width=\(width) height=\(height)")
-    let len = width / 4;
-    let hi = height;
-    let sz = CGSize(width: width, height: height)
-    let renderer = UIGraphicsImageRenderer(size: sz)
-    let image = renderer.image { context in
-        UIColor.red.setFill()
-        context.fill(CGRect(x: 0, y: 0, width: len, height: hi))
-        UIColor.green.setFill()
-        context.fill(CGRect(x: len, y: 0, width: len, height: hi))
-        UIColor.yellow.setFill()
-        context.fill(CGRect(x: len*2, y: 0, width: len, height: hi))
-        UIColor.black.setFill()
-        context.fill(CGRect(x: len*3, y: 0, width: len, height: hi))
-        UIColor.darkGray.setStroke()
-        context.stroke(renderer.format.bounds)
-        var box = renderer.format.bounds
-        for _ in 0...5 {
-            box = box.insetBy(dx: 4, dy: 4)
-            context.stroke(box)
-        }
-        // UIColor(red: 158/255, green: 215/255, blue: 245/255, alpha: 1).setFill()
+  print("renderGraphics width=\(width) height=\(height)")
+  let len = width / 4;
+  let hi = height;
+  let sz = CGSize(width: width, height: height)
+  let renderer = UIGraphicsImageRenderer(size: sz)
+  let image = renderer.image { context in
+    UIColor.red.setFill()
+    context.fill(CGRect(x: 0, y: 0, width: len, height: hi))
+    UIColor.green.setFill()
+    context.fill(CGRect(x: len, y: 0, width: len, height: hi))
+    UIColor.yellow.setFill()
+    context.fill(CGRect(x: len*2, y: 0, width: len, height: hi))
+    UIColor.black.setFill()
+    context.fill(CGRect(x: len*3, y: 0, width: len, height: hi))
+    UIColor.darkGray.setStroke()
+    context.stroke(renderer.format.bounds)
+    var box = renderer.format.bounds
+    for _ in 0...5 {
+      box = box.insetBy(dx: 4, dy: 4)
+      context.stroke(box)
     }
-    return image;
+    // UIColor(red: 158/255, green: 215/255, blue: 245/255, alpha: 1).setFill()
+  }
+  return image;
 }
